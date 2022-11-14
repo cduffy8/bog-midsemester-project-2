e05208062acc0add7580d8d2bb747f77ac85f52b
Connor Duffy <connorpduffy160182@gmail.com>
2022-11-14 10:48:25 -0500

    Added pagination

--- cat-facts/src/App.js
+++ cat-facts/src/App.js
@@ -1,82 +1,44 @@
 import './App.css';
 import { useState } from "react";
+import CatFactCard from './components/CatFactCard';
 
-const api_url = "https://catfact.ninja/fact";
-let facts = [];
 const maxPages = 10;
 
 function App() {
   const [index, setIndex] = useState(0);
-  const [fact, setFact] = useState("Welcome to the Random Fact Cat Generator");
-  const [call, setCall] = useState(true);
+  const [count, setCount] = useState(0);
   const [favorite, setFavorite] = useState('white');
   
   return (
+
     <div className='App-header'>
       <h1>Cat Fact Generator</h1>
       <div style={{
-        color:favorite,
-        padding:'5%',
-      }}>{fact}</div>
-      <div style={{
         display:'flex',
         flexDirection:'row',
       }}>
+
         <button onClick = {() => { 
           if (index > 0) {
             setIndex(index-1);
-            setFact(facts[index-1].fact);
-            setFavorite(facts[index-1].fav);
           }
-        }}>Left</button>
-        <button onClick = {() => {
-          getFact();
-          setCall(!call);
-          setIndex(0);
-          setFact(facts[0].fact);
-          setFavorite("white");
-        }}>New Fact</button>
-        <button onClick = {() => { 
-          if (index + 1 < facts.length) {
-            setIndex(index+1);
-            setFact(facts[index+1].fact);
-            setFavorite(facts[index+1].fav);
-          }
-        }}>Right</button>
-      </div>
-      <div style={{
-        display:'flex',
-        flexDirection:'row',
-      }}>
-        <button onClick = {() => {
-          setFavorite('orange');
-          facts[index].fav = 'orange';
-        }}>Favorite</button>
-        <button onClick = {() => {
-          setFavorite('white');
-          facts[index].fav = 'white';
-        }}>Unfavorite</button>
-      </div>
+        }}>{'<'}</button>
 
+        <button onClick = {() => {
+          setCount(count+1);
+          setIndex(Math.floor((count+1)/10));
+        }}>New Fact</button>
+
+        <button onClick = {() => { 
+          if ((index+1)*maxPages <= count) {
+            setIndex(index+1);
+          }
+        }}>{'>'}</button>
+
+      </div>
+      <CatFactCard index={index} count={count} />
     </div>
   );
 }
-
-function getFact() {
-  fetch(api_url)
-      .then((response) => response.json())
-      .then((data) => updateFacts(data.fact, facts));
-}
-
-function updateFacts(newFact) {
-  facts.unshift({
-    fact:newFact,
-    fav:'white'
-  });
-  if (facts.length > maxPages) {
-    facts.pop();
-  }
-}
-
 
 export default App;
--- cat-facts/src/components/CatFactCard.js
+++ cat-facts/src/components/CatFactCard.js
@@ -1,8 +1,68 @@
+import { useState, useEffect } from "react";
+
+const api_url = "https://catfact.ninja/fact";
+const maxPages = 10;
+
 function CatFactCard(props) {
-    const { index, facts } = props;
+    const { index, count } = props;
+    const [facts, setFacts] = useState([]);
+    const [display, setDisplay] = useState([]);
+
+    useEffect(() => {
+      if (count > facts.length - 1) {
+        fetch(api_url)
+        .then((response) => response.json())
+        .then((data) => {
+          setFacts(
+            [
+              ...facts,
+              {
+                data:data.fact,
+                favorite:'white',
+                id: count+1,
+              } 
+            ]
+          );
+        });
+      }
+      const last = Math.min(index*10 + maxPages, count+1);
+      setDisplay(facts.slice(index*10, last));
+    }, [count, index, facts]);
+
+    console.log(display);
+
     return (
-        <div>
-            {facts[index]}
+        <div style={{
+          display:'flex',
+          flexDirection:'column',
+          justifyContent:'flex-start',
+          padding:'1%',
+        }}>
+            {
+              display.map((disp)=> (
+                <div key={disp.id} style={{
+                  color:disp.favorite,
+                  display:'flex',
+                  flexDirection:'row',
+                  justifyContent:'space-between',
+                }}>
+                    {disp.id + ". " + disp.data}
+                    <button onClick={() => {
+                      const target = disp.id;
+                      setFacts(facts.map((entry) => {
+                        if (entry.id === target) {
+                          if (entry.favorite === 'white') {
+                            return {...entry, favorite:'orange'};
+                          } else {
+                            return {...entry, favorite:'white'};
+                          }
+                        } else {
+                          return entry;
+                        }
+                      }));
+                    }}>Favorite</button>
+                </div>
+            ))}
         </div>
     );
 }
